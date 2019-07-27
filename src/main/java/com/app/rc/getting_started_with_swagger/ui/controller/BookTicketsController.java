709c6504c30471e3b585bdded194d82633eb7836
iHSPA <harrsh.hp@live.iin>
2019-07-27 17:48:46 +0530

    App-Log::ApiParam-Added

--- src/main/java/com/app/rc/getting_started_with_swagger/ui/controller/BookTicketsController.java
+++ src/main/java/com/app/rc/getting_started_with_swagger/ui/controller/BookTicketsController.java
@@ -5,12 +5,10 @@ import com.app.rc.getting_started_with_swagger.ui.model.request.BookTicketsReque
 import com.app.rc.getting_started_with_swagger.ui.model.response.BookTicketsResponseModel;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -19,15 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookTicketsController {
 
     /**
-     * FIXME: Service implementation missing
+     * Accepts a booking data and returns a Booking ID on successful operation
      *
      * @param request Valid request to create a ticket booking
      * @return Response object container data used to create ticket booking
      */
-    @ApiOperation(value = "Book a Ticket",
+    @ApiOperation(
+            value = "Book a Ticket",
             notes = "Accepts a booking data and returns a Booking ID on successful operation")
     @PostMapping(path = "/book", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public BookTicketsResponseModel bookTickets(@RequestBody BookTicketsRequestModel request) {
+    public BookTicketsResponseModel bookTickets(
+            @ApiParam(
+                    value = "Request Payload for Ticket",
+                    example = "Booking Details of the Ticket: id:1, show:Sherlock Holmes, etc.")
+            @RequestBody BookTicketsRequestModel request) {
 
         BookTicketsDTO dto = new BookTicketsDTO();
         BookTicketsResponseModel response = new BookTicketsResponseModel();
@@ -36,5 +39,27 @@ public class BookTicketsController {
         BeanUtils.copyProperties(dto, response);
 
         return response;
+    }
+
+    /**
+     * Get Booking data for the given Booking ID
+     *
+     * @param id UUID for Booking
+     * @return Booking data object
+     */
+    @ApiOperation(
+            value = "Get booking data for the given Booking ID",
+            produces = "BookTicketsResponseModel"
+    )
+    @GetMapping(path = "/booking/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public BookTicketsResponseModel getTickets(
+            @ApiParam(
+                    value = "Booking ID for the Ticket",
+                    example = "3kjdhj34oiu93",
+                    type = "String",
+                    required = true
+            )
+            @RequestParam String id) {
+        return new BookTicketsResponseModel();
     }
 }
